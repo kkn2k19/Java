f61bbfed04f949278bda3bf597b8c25407c081e3
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2024-03-22 23:29:13 +0530

    22.03.2024

---
+++ Java.KK/FloorOfaNumber.java
@@ -0,0 +1,27 @@
+// Floor of a Number -- ( greatest element in array smaller or equal to target.)
+
+public class FloorOfaNumber {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
+        int key = 17; // enter values lesser than or equal to 18.
+        System.out.println(floorOfaNumber(arr, key));
+    }
+
+    static int floorOfaNumber(int[] arr, int key) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (key == arr[mid]) {
+                return key;
+            }
+            if (key < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return arr[end];
+    }
+}
