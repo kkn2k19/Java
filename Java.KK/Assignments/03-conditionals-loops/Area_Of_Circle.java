32824a8cae0805919960b05f17a55e081ca2e7d4
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2024-04-02 21:25:24 +0530

    Update and rename AreaOfCircle.java to Area_Of_Circle.java

--- Java.KK/Assignments/03-conditionals-loops/Area_Of_Circle.java
+++ Java.KK/Assignments/03-conditionals-loops/Area_Of_Circle.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class AreaOfCircle {
+public class Area_Of_Circle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Radius of Circle : ");
