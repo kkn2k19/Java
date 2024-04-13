c5411f2fe5ab32a0fa517324a109c3485584f6d2
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2024-04-14 02:38:52 +0530

    Update FactorialOfNumber.java

--- Java.KK/Assignments/04-functions/FactorialOfNumber.java
+++ Java.KK/Assignments/04-functions/FactorialOfNumber.java
@@ -1,4 +1,4 @@
-// Write a program to print the factorial of a number by defining a method named
+// 9. Write a program to print the factorial of a number by defining a method named
 // 'Factorial'. Factorial of any number n is represented by n! and is equal to 1
 // * 2 * 3 * .... * (n-1) *n. E.g.-
 /*
