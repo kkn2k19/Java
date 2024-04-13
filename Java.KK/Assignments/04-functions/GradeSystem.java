1500545c744f901a5879b0587f70f266375c2dd9
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2024-04-14 02:37:52 +0530

    Update GradeSystem.java

--- Java.KK/Assignments/04-functions/GradeSystem.java
+++ Java.KK/Assignments/04-functions/GradeSystem.java
@@ -1,5 +1,5 @@
 // package Assignments.04-functions;
-// Write a program that will ask the user to enter his/her marks (out of 100).Define a method that will display grades according to the marks entered as below : 
+// 8. Write a program that will ask the user to enter his/her marks (out of 100).Define a method that will display grades according to the marks entered as below : 
 /*
  *      Marks           Grade
  *      91-100          AA
