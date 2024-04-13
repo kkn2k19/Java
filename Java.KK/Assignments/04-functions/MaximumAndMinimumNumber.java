e63d6a7708ac91825875ab88273dde5486f11185
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2024-04-14 02:25:48 +0530

    Update MaximumAndMinimumNumber.java

--- Java.KK/Assignments/04-functions/MaximumAndMinimumNumber.java
+++ Java.KK/Assignments/04-functions/MaximumAndMinimumNumber.java
@@ -1,5 +1,6 @@
 // 28.03.2024
 // https://www.java67.com/2019/05/how-to-find-largest-and-smallest-of-three-numbers-in-java.html
+// 1. Define two methods to print the maximum and the minimum number respectively among three numbers entered by the user.
 
 import java.util.Scanner;
 
