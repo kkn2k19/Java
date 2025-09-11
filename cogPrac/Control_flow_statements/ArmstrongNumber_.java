a0ad8be66458520d406ef9db515da10411e6a8ab
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-09-11 20:44:20 +0530

    .

--- cogPrac/Control_flow_statements/ArmstrongNumber_.java
+++ cogPrac/Control_flow_statements/ArmstrongNumber_.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ArmstrongNumber {
+public class ArmstrongNumber_ {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
--- cogPrac/Control_flow_statements/PrimesInRange_.java
+++ cogPrac/Control_flow_statements/PrimesInRange_.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PrimesInRange {
+public class PrimesInRange_ {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
